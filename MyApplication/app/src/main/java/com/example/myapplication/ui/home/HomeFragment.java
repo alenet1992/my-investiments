44d3267265c47041e6ce84f4640b93c833101b8e
Alexandre Tiago Teixeira Pereira <alexandre.pereira@prozis.com>
2019-11-16 13:02:33 +0000

    Start of project

---
+++ MyApplication/app/src/main/java/com/example/myapplication/ui/home/HomeFragment.java
@@ -0,0 +1,34 @@
+package com.example.myapplication.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.myapplication.R;
+
+public class HomeFragment extends Fragment {
+
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+
+        WebView webView=(WebView) root.findViewById(R.id.webview1);
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("https://www.mintos.com/en/login");
+
+        return root;
+    }
+}
